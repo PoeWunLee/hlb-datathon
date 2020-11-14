73756748ad75978d954dbe2e5f8b545ae23e9b72
Jun Kwan <kwanjunwen@gmail.com>
2020-11-14 22:41:05 +1100

    Add pca load_digits

--- samples/04-principal-component-analysis.py
+++ samples/04-principal-component-analysis.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns; 
 from sklearn.decomposition import PCA
+from sklearn.datasets import load_digits
 
 sns.set()
 rng = np.random.RandomState(1)
@@ -25,4 +26,18 @@ X_new = pca.inverse_transform(X_pca)
 plt.scatter(X[:, 0], X[:, 1], alpha=0.2)
 plt.scatter(X_new[:, 0], X_new[:, 1], alpha=0.8)
 plt.axis('equal')
+plt.show()
+
+digits = load_digits()
+pca = PCA(2)  # project from 64 to 2 dimensions
+projected = pca.fit_transform(digits.data)
+print(digits.data.shape)
+print(projected.shape)
+
+plt.scatter(projected[:, 0], projected[:, 1],
+            c=digits.target, edgecolor='none', alpha=0.5,
+            cmap=plt.cm.get_cmap('gist_rainbow_r',10))
+plt.xlabel('component 1')
+plt.ylabel('component 2')
+plt.colorbar()
 plt.show()
