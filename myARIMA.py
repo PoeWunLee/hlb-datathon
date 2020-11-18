f3c99d3dee6c7837bcfee67d68e2350e8b584e23
Khoo Kian Chong <kian.chong.khoo@gmail.com>
2020-11-19 00:39:07 +0800

    Fixed ARIMA

--- myARIMA.py
+++ myARIMA.py
@@ -3,16 +3,12 @@ from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
 
-def thisARIMA(myDF, iterDict, rolling_average, num_forecast):
+def thisARIMA(past_values, mth, rolling_average, num_forecast):
     '''
     Returns time series forecasting value
         Parameters:
-            myDF (data frame): cleaned master dataframe
-            iterDict (dictionary): dictionary of user's input
-                                    for eg:  iterDict = {"Residential_Type": restype,
-                                                         "Property_State": state,
-                                                         "Property_Type": proptype,
-                                                          "Landed_Type": land}
+            past_values (list): previous average values
+            mth (list) : previous months
             num_forecast (int): number of forecast
             rolling_average (list): rolling average of the average of difference
         Returns:
@@ -20,22 +16,17 @@ def thisARIMA(myDF, iterDict, rolling_average, num_forecast):
             output_y (list) = rolling average + forecast value (y value of the plot)
     }
     '''
-
-    for key, value in iterDict.items():
-        myDF = myDF.loc[myDF[key] == value]
-
-    sumdf = myDF.groupby(["Submission_Mth"]).mean().reset_index()
-
-    model = SARIMAX(sumdf.difference, order=(1, 1, 1), seasonal_order=(0, 0, 0, 0))
+    model = SARIMAX(past_values, order=(1, 1, 1), seasonal_order=(0, 0, 0, 0))
     results = model.fit(disp=-1)
     forecast = results.forecast(steps=num_forecast)
 
-    mth = [datetime.strptime(date, '%Y-%m') for date in sumdf.Submission_Mth.tolist()]
+    mth = [datetime.strptime(date, '%Y-%m') for date in mth]
 
     for _ in range(num_forecast):
         mth.append(mth[-1] + relativedelta(months=1))
 
-    output_y = rolling_average + forecast.tolist()
+    output_y = forecast.tolist()
+    mth = mth[-len(output_y):]
 
     return mth, output_y
 
